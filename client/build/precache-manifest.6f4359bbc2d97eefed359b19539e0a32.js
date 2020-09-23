662026923b3e23b9fe5c519bd64e44d63e880e48
winnni <dzidoulapatron1@gmail.com>
2020-09-23 08:59:12 +0000

    create build folder in client

---
+++ client/build/precache-manifest.6f4359bbc2d97eefed359b19539e0a32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6ee429606bfdf535e7a217d6f1b9543",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1154550de45472623520",
+    "url": "/static/css/main.fdbafa3e.chunk.css"
+  },
+  {
+    "revision": "49ac16c4e9c6cb0ad7f9",
+    "url": "/static/js/2.88ce2164.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/static/js/2.88ce2164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1154550de45472623520",
+    "url": "/static/js/main.6b307bf9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 const path = require('path');
-const publicPath = path.join(__dirname, '.', 'public');
+const publicPath = path.join(__dirname, '.', 'client/build');
 //connect to database
 connectDB();
 //init middleware
@@ -13,7 +13,7 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/users', require('./routes/api/users'));
-
+// charger le serveur
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
@@ -30,7 +30,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 */
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Le serveur a demarrer au port ${PORT}`));
